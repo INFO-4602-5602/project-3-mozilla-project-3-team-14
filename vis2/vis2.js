1a206a971bde417566fa5dad2f0b60ed6ba5bfc3
Brian Lubars <blubars@gmail.com>
2018-04-05 03:35:36 -0600

    vis2 progress

--- vis2/vis2.js
+++ vis2/vis2.js
@@ -1,297 +1,240 @@
+//
+// Script to draw Vis 2
+// 4/4/2018
+//
+var dataFile = "../data/mozilla-filt-coded.csv"
+// GeoJSON converted from public-domain shape file from Natural Earth Data
+// http://www.naturalearthdata.com/
+var geoFile = "./110m_countries_2.json"
+
 // set the dimensions and margins of the graph
 var margin = {top: 40, right: 40, bottom: 40, left: 40};
-var width = 800 - margin.left - margin.right;
-var height = 800 - margin.top - margin.bottom;
+var width = 880 - margin.left - margin.right
+var height = 500 - margin.top - margin.bottom;
+var map_size = {width: 800, height: 420};
+var svg_size = {margin: margin, width: width, height: height};
+var data;
 
-var colorScale = d3.scaleOrdinal(d3['schemeCategory20c']);
+// create color scale
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var xVal = "year";
-var yVal = "percent";
+var fearField = "What is your biggest fear as we move towards a more connected future?";
+var fearsMapping = [
+    "The loss of privacy", 
+    "We'll lose touch with one another", 
+    "I have no fears about a more connected future", 
+    "We'll be less safe", "Other (please specify)"];
+var exciteField = "What are you most excited about as we move toward a more digitally connected future?";
 
-// these functions are the accessors for each element in the dataset
-var getX = function(d) { return +d[xVal]; };
-var getY = function(d) { return +d[yVal]; };
+/* Run everything! */
+function main() {
+    // add svg to canvas, set size
+    d3.select("#map").append("svg")
+        .attr("width", map_size.width)
+        .attr("height", map_size.height);
 
-// these are our scales for the axes
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-// these functions retrieve the appropriate values for the given
-// element for the appropriately set scale
-var getScaledX = function(d) { return x(d[xVal]); };
-var getScaledY = function(d) { return y(d[yVal]); };
-
-var svg;
-
-function parse_data(data) {
-    var parsed_rows = [];
-    var by_major = {};
-
-    // format the data properly
-    for (var i in data) {
-        var row = data[i];
-
-        if (row.year && row.major) {
-            row.year = +row.year;
-            row.percent = parseFloat(row.percent).toFixed(3);
-
-            parsed_rows.push(row);
-
-            if (! (row.major in by_major)) {
-                by_major[row.major] = [];
-            }
-
-            by_major[row.major].push(row);
-        }
-    }
-
-    // remove stuff that doesn't have at least 10 years
-    for (var major in by_major) {
-        if (by_major[major].length <  10) {
-            delete by_major[major];
-        }
-    }
-
-    var majors = {};
-    var i = 0;
-    for (var major in by_major) {
-        majors[major] = i;
-        i++;
-    }
-
-    // remove invalid majors from the data
-    parsed_rows = parsed_rows.filter(row => row.major in majors);
-
-    return {
-        'data' : parsed_rows,
-        'by_major' : by_major,
-        'majors' : majors,
-    };
+    /* need to load both JSON and CSV before we can go. */
+    d3.queue()
+        /* Load the GeoJSON: JSON format for specifying geographic data:
+         * used to draw the map. geoJSON taken from: */
+        .defer(d3.json, geoFile)
+        /* This is the data loader function: load data from the CSV and
+         * set up the HTML with an SVG */
+        .defer(d3.csv, dataFile)
+        /* execute this function when both above done! */
+        .await(processData);
 }
 
-d3.csv("./data.csv", function(error, data) {
-    var parsed = parse_data(data);
-    var data = parsed.data;
-    var by_major = parsed.by_major;
-    var majors = parsed.majors;
+function processData(error, geo, rawdata) {
+    if (error) throw error;
+    // group our data by country
+    data = d3.nest()
+        .key(function(d) { return d.Country; })
+        // rollup: replace indiv records within group by one record
+        .rollup(sumarizeCountry)
+        .object(rawdata);
+    console.log(data)
 
-    // scale things
-    x.domain(d3.extent(data, getX));
-    y.domain([0, 1]);
+    drawMap(geo, data);
+}
 
-    svg = getSVGWithLabelsAndAxes('#vis2', x, y, "Percentage of Women in different majors over time");
-
-    var line = d3.line()
-        .x(getScaledX)
-        .y(getScaledY);
-
-    // gridlines code credit to: 
-    // https://bl.ocks.org/d3noob/c506ac45617cf9ed39337f99f8511218
-    // gridlines in x axis function
-    function make_x_gridlines() {		
-        return d3.axisBottom(x)
-            .ticks(5)
+function getCountryData(country) {
+    if (data[country] == undefined) {
+        console.log("Country not found");
+        return null;
+    } else {
+        return data[country];
     }
+}
 
-    // gridlines in y axis function
-    function make_y_gridlines() {		
-        return d3.axisLeft(y)
-            .ticks(5)
+/* this function returns a color to fill the svg path for a country,
+ * based on the given attribute (maximum fears or excitements). */
+function getCountryAttrColor(country, attribute) {
+    if (data[country] == undefined) {
+        console.log("Country not found");
+        console.log(country);
+        return "#ddd";
+    } else {
+        d = data[country][attribute];
+        props = Object.keys(d);
+        max = props[0];
+        for (var i = 0; i < props.length; i++) {
+            if (d[props[i]] > d[max]) {
+                max = props[i];
+            }
+        }
+        return color(max);
     }
+}
 
-    // add the X gridlines
-    svg.append("g")			
-        .attr("class", "grid")
-        .attr("transform", "translate(0," + height + ")")
-        .call(make_x_gridlines()
-            .tickSize(-height)
-            .tickFormat("")
-        )
+function sumarizeCountry(entries) {
+    var filt = entries.filter(function(d) { 
+            return (d[fearField] != "") && (d[exciteField] != ""); })
+    /* count each response for fears and excitements */
+    fear = filt.reduce(function(sum, entry) {
+        d = +entry[fearField];
+        if (sum[d] == undefined) {
+            sum[d] = 1;
+        } else {
+            sum[d] += 1;
+        }
+        return sum;
+    }, {});
+    exc = filt.reduce(function(sum, entry) {
+        d = +entry[exciteField];
+        if (sum[d] == undefined) {
+            sum[d] = 1;
+        } else {
+            sum[d] += 1;
+        }
+        return sum;
+    }, {});
 
-    // add the Y gridlines
-    svg.append("g")			
-        .attr("class", "grid")
-        .call(make_y_gridlines()
-            .tickSize(-width)
-            .tickFormat("")
-        )
+    return {"Feared":fear, "Excited":exc};
+}
 
-    for (var major in majors) {
-        info = by_major[major].sort(function(a, b){return a[xVal] - b[xVal]});
+/* This function draws the world map projection, coloring each country
+ * according to the given attribute */
+//function drawMap(data, attribute) {
+function drawMap(geojson, data) {
+    console.log(geojson);
+    // projection: function that converts from long/lat to x/y
+    //var projection = d3.geoEquirectangular()
+    var projection = d3.geoNaturalEarth1()
+        .scale(150) // default scale is 150
+        .translate([map_size.width / 2, (map_size.height / 2)])
+        .precision(.1);
+    // generator creates svg path from projection
+    var generator = d3.geoPath()
+        .projection(projection);
+    // add path to svg
+    var svg = d3.select('#map svg');
 
-        svg.append("path")
-            .data([info])
-            .attr("class", "vis2-line " + getMajorAsClass(major))
-            .attr("d", line)
-            .attr("stroke", colorScale(majors[major]))
-            .attr("stroke-width", 3)
-            .style("fill", 'none')
-            .on("mouseover", lineMouseOver)
-            .on("mouseout", lineMouseOut);
-    }
-
-    svg.selectAll(".vis2-dot")
-        .data(data)
+    // draw longitude/lattitude lines
+    svg.append('g')
+        .attr("class", "graticule")
+        .selectAll('path')
+        .data(d3.geoGraticule().lines)
         .enter()
-        .append("circle")
-        .attr("class", function(d, i) { return "vis2-dot " + getMajorAsClass(d.major) })
-        .attr("cx", getScaledX)
-        .attr("cy", getScaledY)
-        .attr("r", 4)
-        .attr("fill", function(d, i) { return colorScale(majors[d.major]); })
-        .on("mouseover", dotMouseOver)
-        .on("mouseout", dotMouseOut);
+        .append("path")
+        .attr("d", generator)
 
-});
+    // draw countries
+    svg.append('g')
+        .attr("class", "country")
+        .selectAll('path')
+        .data(geojson.features)
+        .enter()
+        .append('path')
+        .attr('d', generator)
+        .style("fill", function(d) { 
+            return getCountryAttrColor(d.properties.NAME_LONG, 'Feared'); })
+        .on("mouseover", handleCountryMouseoverEvent)
+        .on("mouseout", handleCountryMouseoutEvent)
+        .on("click", handleCountryClickEvent);
 
-// this function isolates the way we turn a major into an html class
-function getMajorAsClass(major) {
-    return major.replace(/ /g,'');
+    var legend = d3.legendColor()
+        .shape('circle')
+        .labels(fearsMapping)
+        .scale(color);
+
+    // add a color key
+    svg.append("g")
+        .attr("class", "key")
+        .attr("transform", "translate(12,320)")
+        .call(legend);
+
 }
 
-// this function takes a class name, a major, and an opacity
-// and changes all elements of the given classname without the 
-// given major to the new opacity.
-function changeElementOpacityForDifferentMajors(class_name, major, new_opacity) {
-    var elements = document.getElementsByClassName(class_name);
+function handleCountryMouseoverEvent(d, i) {
+    // outline on mousover
+    d3.select(this)
+      .style("stroke", "gold")
+      .style("stroke-width", "2px");
+    // make tooltip appear, set text
+    d3.select("body #tooltip")
+      .style("opacity", 0.9)
+      .style("left", (d3.event.pageX + 5) + "px")
+      .style("top", (d3.event.pageY + 5) + "px");
+    d3.select("body #tooltip p")
+      .text(d.properties.NAME);
+}
 
-    for (var i in elements) {
-        var element = elements[i];
-        // var classes = element.className;
+function handleCountryMouseoutEvent(d, i) {
+    // outline on mousover
+    if(this != activeCountryItem) {
+        d3.select(this)
+            .style("stroke", "#aaa")
+            .style("stroke-width", "1px");
+    }
+    d3.select("body #tooltip")
+        .style("opacity", 0)
+        .style("left", "0px")
+        .style("top", "0px");
+}
 
-        if (element.className) {
-            var classes = element.className.baseVal.split(" ");
-
-            alt_major = classes.filter(c => c != class_name)[0];
-
-            if (alt_major != major) {
-                element.style.opacity = new_opacity;
-            }
+activeCountryItem = null;
+/* function to handle the clicking of a country. Transition that
+ * country to active state: 
+ * highlight it, and display its stats */
+function handleCountryClickEvent(d, i) {
+    // deselect old country
+    d3.select(activeCountryItem)
+        .style("stroke", "#aaa")
+        .style("stroke-width", "1px");
+    // select new country
+    activeCountryItem = this;
+    d3.select(this)
+        .style("stroke", "gold")
+        .style("stroke-width", "2px");
+    d3.select("#countryInfo h3")
+        .text(d.properties.NAME);
+    nums = getCountryData(d.properties.NAME_LONG);
+    var totalResponses = 0
+    if (nums != null) {
+        for (prop in nums['Feared']) {
+            totalResponses += nums['Feared'][prop];
         }
     }
-}
-
-// this function:
-// - makes everything not from this line's major translucent
-// - adds a div describing the major being shown
-function lineMouseOver(l, i) {
-    var major = getMajorAsClass(l[0].major);
-
-    changeElementOpacityForDifferentMajors("vis2-line", major, '.1');
-    changeElementOpacityForDifferentMajors("vis2-dot", major, '.1');
-
-    // this makes the tooltip visible
-    div.transition()
-        .duration(50)
-        .style("opacity", .9);
-
-    var string = "Women majoring in <i>" + l[0]['major'] + '</i>';
-
-    // this sets the location and content of the tooltip to the point's location/coordinates
-    div.html(string)
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-}
-
-// this function:
-// - makes everything not from this line's major non-translucent
-// - hides a div describing the major being shown
-function lineMouseOut(l, i) {
-    var major = getMajorAsClass(l[0].major);
-
-    changeElementOpacityForDifferentMajors("vis2-line", major, '1');
-    changeElementOpacityForDifferentMajors("vis2-dot", major, '1');
-
-    div.transition()
-        .duration(100)
-        .style("opacity", 0);
-}
-
-// this div will be hidden at first, but will appear on hover to show
-// tooltips
-var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// this function does two things:
-// 1. it makes the radius a dot bigger
-// 2. it makes a tooltip appear which displays the x and y coordinates of the point
-function dotMouseOver(d, i) {
-    d3.select(this).attr('r', '10');
-
-    // this makes the tooltip visible
-    div.transition()
-        .duration(50)
-        .style("opacity", .9);
-
-    var percent = 100 * d[yVal];
-    percent = percent.toFixed(1);
-
-    var string = "Women made up " + percent + "% of the <i>" + d['major'] + '</i> major in ' + d[xVal];
-
-    // this sets the location and content of the tooltip to the point's location/coordinates
-    div.html(string)
-        .style("left", (d3.event.pageX) + "px")
-        .style("top", (d3.event.pageY - 28) + "px");
-}
-
-// this function does two things:
-// 1. it resets the radius of a dot
-// 2. it makes the tooltip invisible.
-function dotMouseOut(d, i) {
-    d3.select(this).attr('r', '4');
-
-    div.transition()
-        .duration(100)
-        .style("opacity", 0);
-}
-
-// this function takes in three parameters:
-// 1. an element to append an SVG to
-// 2. an x-axis
-// 3. a y-axis
-// 
-// it does the gruntwork of:
-// - making the svg the right size
-// - adding the x- and y-axes to the graph
-// - labelling those axes
-function getSVGWithLabelsAndAxes(element, x_axis, y_axis, label) {
-    var svg = d3.select(element).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate("+ margin.left + "," + margin.top + ")");
-
-    svg.append("text")
-        .attr("class", "label")
-        .text(xVal)
-        .attr("x", width + 10)
-        .attr("y", height);
-
-    // y-axis label
-    svg.append("text")
-        .attr("class", "label")
-        .text(yVal)
-        .attr("y", - 10);
-
-    // x-axis
+    d3.select("#countryInfo p")
+        .text("Total Responses: " + totalResponses);
+    // make bar chart
+    svg = d3.select("#countryInfo svg")
+    var x = d3.scaleLinear().rangeRound([0, 150]);
+    var y = d3.scaleLinear().rangeRound([100, 0]);
+    x.domain(fearsMapping);
+    y.domain([0, d3.max(data, function(d) { return d; } )]);
     svg.append("g")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(d3.axisBottom(x_axis));
-
-    // y-axis
-    svg.append("g")
-        .call(d3.axisLeft(y_axis));
-
-    if (label) {
-        svg.append("text")
-            .attr("x", (width / 2))             
-            .attr("y", 0 - (margin.top / 3))
-            .attr("text-anchor", "middle")  
-            .style("font-size", "16px") 
-            .style("text-decoration", "underline")  
-            .text(label);
-    }
-
-    return svg;
+        .selectAll(".bar")
+        .data(nums['Feared'])
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d); })
+        .attr("y", function(d) { return y(d); })
+        .attr("width", "20")
+        .attr("height", function(d) { return 100 - y(d); });
 }
+
+/* run the script! */
+main();
+
